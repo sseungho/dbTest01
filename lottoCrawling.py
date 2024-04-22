4382be43d6229a36ad6ba736a235ad3c31543222
sseungho <bedder9antwerp@hotmail.com>
2024-04-22 11:45:22 +0900

    sqlAlchemy 로 데이터프레임을 db 테이블로만들기 예제 추가

--- lottoCrawling.py
+++ lottoCrawling.py
@@ -1,8 +1,10 @@
+from functools import cache
 import requests
 from bs4 import BeautifulSoup
 from datetime import datetime
 import pandas as pd
 
+@cache
 def get_lottoNumber(count): # lotto 추첨 회차를 인수로 받음
     url = f"https://dhlottery.co.kr/gameResult.do?method=byWin&drwNo={count}"
     html = requests.get(url).text
@@ -19,4 +21,29 @@ def get_lottoNumber(count): # lotto 추첨 회차를 인수로 받음
     # 로또 당첨 보너스 번호 1개 반환 >> str 을 int 변환
     bonusNumber = int(soup.find('div', {'class': 'num bonus'}).find('p').text.strip())
 
-    return
+    lottoDic = {'lottoDate': lottoDate, 'lottoNumber': lottoNumberList, 'bonusNumber': bonusNumber}
+
+    return lottoDic
+
+lottoDf_list = []
+
+for count in range(1, 1117):
+    lottoResult = get_lottoNumber(count)
+
+    lottoDf_list.append({
+        'count': count,    # lotto 추첨 회차
+        'lottoDate': lottoResult['lottoDate'], # lotto 추첨일
+        'lottoNum1': lottoResult['lottoNumber'][0], # 1번째 당첨 번호
+        'lottoNum2': lottoResult['lottoNumber'][1],  # 2번째 당첨 번호
+        'lottoNum3': lottoResult['lottoNumber'][2],  # 3번째 당첨 번호
+        'lottoNum4': lottoResult['lottoNumber'][3],  # 4번째 당첨 번호
+        'lottoNum5': lottoResult['lottoNumber'][4],  # 5번째 당첨 번호
+        'lottoNum6': lottoResult['lottoNumber'][5],  # 6번째 당첨 번호
+        'bonusNum': lottoResult['bonusNumber']      # bonus 번호
+    })
+    print(f"{count} 회차 처리중......")
+
+lottoDf = pd.DataFrame(data = lottoDf_list, columns = ['count', 'lottoDate',
+            'lottoNum1', 'lottoNum2','lottoNum3','lottoNum4','lottoNum5','lottoNum6','bonusNum'])
+
+print(lottoDf)
---
+++ sqlAlchemyTest.py
@@ -0,0 +1,15 @@
+from sqlalchemy import create_engine
+import pandas as pd
+import pymysql
+
+data = {'학번': range(2000,2010),'성적':[55,66,77,22,33,55,34,88,99,100]}
+
+df= pd.DataFrame(data = data, columns=['학번','성적'])
+
+print(df)
+
+engine = create_engine("mysql+pymysql://root:12345@localhost:3306/lottodb?charset=utf8mb4")
+engine.connect()
+
+df.to_sql(name="test_tbl", con=engine, if_exists='append', index=False)
+
